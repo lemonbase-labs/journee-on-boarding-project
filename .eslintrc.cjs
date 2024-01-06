35aeb359e30fd4a7535dbfd83fa2158e5398e680
Jisoo Yoo <youzysu@gmail.com>
2024-01-06 17:08:45 +0900

    chore: 프로젝트 기본 개발환경 설정
    - 필수 패키지 설정
    - github PR 템플릿 추가

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:react-hooks/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react/recommended',
+    'prettier',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'react/self-closing-comp': [
+      'error',
+      {
+        component: true,
+        html: true,
+      },
+    ],
+  },
+};
